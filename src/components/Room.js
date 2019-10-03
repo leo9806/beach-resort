4f335adac0b016f5ec380d5706f0323c9adcba05
Leo <antoniou.leo@gmail.com>
2019-10-03 20:04:57 +0100

    Implementation of Room component
    Used o display the rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+// acts as an alt image for the '<img>'
+import DefaultImg from '../images/room-1.jpeg';
+import PropTypes from 'prop-types';
 
-const Room = () => {
+const Room = ({room}) => {
+  // destructuring the room
+  const{name, dang, images, price} = room;
+
+
   return (
-    <div>
-      hello from room!
-    </div>
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || DefaultImg} alt="single room" />
+        <div className="price-top">
+          <h6>£{price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${dang}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
   )
 }
 
-export default Room
+Room.propTypes = {
+  // checking if this object has particular properties
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    dang: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired
+
+  })
+};
+
+export default Room;
