04ee1bafd52165ac22f06a12acf1416807be23b2
Leo <antoniou.leo@gmail.com>
2019-10-06 23:31:50 +0100

    SingleRoom page completed
    Added description, details and extras sections

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,7 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 import {RoomContext} from '../context';
+import StyledHero from '../components/StyledHero';
 
 export default class SingleRoom extends Component {
   // the props are passed by react router
@@ -38,17 +39,62 @@ export default class SingleRoom extends Component {
     const {name, description, capacity, size, price, extras,
        breakfast, pets, images} = room;
     
-       return <Hero hero="roomsHero" >
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            Back to rooms
-          </Link>
-        </Banner>
-      </Hero>
+    // destructuring the array to get the first image of the array (mainImg)
+    // and another array with the rest of the images of the array
+    const [mainImg, ...defaultImg] = images;
+
     return (
-      <div>
-        Hello from single room page {room.name}
-      </div>
+      <>
+        <StyledHero img={mainImg || this.state.DefaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />
+            } 
+            )}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>
+                {description}
+              </p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: £{price}</h6>
+              <h6>size: {size}SQFT</h6>
+              {/* conditional rendering using a turnary operator */}
+              <h6>
+                max capacity: {
+                  capacity > 1 ? `${capacity} people` : 
+                    `${capacity} person`
+                }
+              </h6>
+              <h6>
+                {pets ? "pets allowed" : "now pets allowed"}
+              </h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>
+                - {item}
+              </li>
+            })}
+          </ul>
+        </section>
+      </>
     )
   }
 }
