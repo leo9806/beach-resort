505c826b83f032db1ea1b836fc8f5a251d7e51b2
Leo <antoniou.leo@gmail.com>
2019-10-07 20:36:46 +0100

    Changed the way the api key is received

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,9 @@
 import {createClient} from 'contentful';
+
+require('dotenv').config();
+
 // accessing the contentful api
 export default createClient({
-  space: "13jv0e7zcnb8",
-  accessToken: "g529_uVRBgjUa56_EcHctuY4y_ak5sAx8SCNuMkBql8"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
